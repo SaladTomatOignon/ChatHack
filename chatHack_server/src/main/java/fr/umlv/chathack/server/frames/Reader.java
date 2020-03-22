6c489861470d468a3b46a08aa53f71b9dd098738
afkeu <37948037+afkeu@users.noreply.github.com>
2020-03-22 22:05:40 +0100

    Add files via upload

---
+++ chatHack_server/src/main/java/fr/umlv/chathack/server/frames/Reader.java
@@ -0,0 +1,13 @@
+package fr.umlv.chathack.server.readers;
+
+public interface Reader {
+
+    public static enum ProcessStatus {DONE,REFILL,ERROR};
+
+    public ProcessStatus process();
+
+    public Object get();
+
+    public void reset();
+
+}
