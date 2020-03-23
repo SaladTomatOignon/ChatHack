c5df158d1fe7714932b2bf99f3501d8e9bef3dd9
afkeu <37948037+afkeu@users.noreply.github.com>
2020-03-23 19:17:26 +0100

    Add files via upload

---
+++ chatHack_client/src/main/java/fr/umlv/chathack/client/frames/ErrorFrame.java
@@ -0,0 +1,54 @@
+package fr.umlv.chathack.client.frames;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class ErrorFrame implements Frame{
+
+	private byte infoCode;
+	private String message;
+	
+	
+	
+	
+	public ErrorFrame(byte infoCode, String message) {
+		this.infoCode = infoCode;
+		this.message = message;
+	}
+
+
+
+
+	public void accept() {
+		System.out.println("infoCode : " + infoCode + " message : " + message);
+	}
+	
+	@Override
+	public byte[] getBytes() {
+		var cs = StandardCharsets.UTF_8;
+		var bb = ByteBuffer.allocate(1024);
+		var messageEncode = cs.encode(message);
+		
+		bb.put((byte) 4 );
+		
+		bb.put(infoCode);
+		
+		bb.putInt(messageEncode.remaining());
+		bb.put(messageEncode);
+		
+		bb.flip();
+		byte[] arr = new byte[bb.remaining()];
+		bb.get(arr);
+		return arr;
+	}
+	
+	@Override
+	public int size() {
+		var cs = StandardCharsets.UTF_8;
+		
+		return cs.encode(message).remaining() + 2 + 4;
+		
+
+	}
+	
+}
