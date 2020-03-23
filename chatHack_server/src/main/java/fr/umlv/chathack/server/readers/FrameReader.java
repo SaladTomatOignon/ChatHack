c5781f583603d5aad0454764bb1f855f70cecf9f
afkeu <37948037+afkeu@users.noreply.github.com>
2020-03-23 19:17:01 +0100

    Add files via upload

--- chatHack_server/src/main/java/fr/umlv/chathack/server/readers/FrameReader.java
+++ chatHack_server/src/main/java/fr/umlv/chathack/server/readers/FrameReader.java
@@ -1,6 +1,9 @@
 package fr.umlv.chathack.server.readers;
 
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 import fr.umlv.chathack.server.frames.Frame;
 
@@ -24,19 +27,26 @@ public class FrameReader implements Reader{
 	
 	private Frame frame;
 	
+	private final Map<Byte, Supplier<ProcessStatus>> map = new HashMap<Byte, Supplier<ProcessStatus>>();
+	
+
+	public FrameReader(ByteBuffer bb, ConnectionReader connectionReader, PublicMessageReader publicMessageReader,
+			PrivateRequestReader privateRequestReader, PrivateAnswerReader privateAnswerReader) {
+		super();
+		this.bb = bb;
+		this.connectionReader = connectionReader;
+		this.publicMessageReader = publicMessageReader;
+		this.privateRequestReader = privateRequestReader;
+		this.privateAnswerReader = privateAnswerReader;
+		map.put((byte) 0, () -> processReader(connectionReader));
+		map.put((byte) 1, () -> processReader(publicMessageReader));
+		map.put((byte) 2, () -> processReader(privateRequestReader));
+		map.put((byte) 3, () -> processReader(privateAnswerReader));
+	}
 
 	public FrameReader(ByteBuffer bb) {
-		this.bb = bb;
-		connectionReader = new ConnectionReader(bb);
-		publicMessageReader = new PublicMessageReader(bb);
-		privateRequestReader = new PrivateRequestReader(bb);
-		privateAnswerReader = new PrivateAnswerReader(bb);
+		this(bb, new ConnectionReader(bb), new PublicMessageReader(bb), new PrivateRequestReader(bb), new PrivateAnswerReader(bb));
 	}
-	
-//	public static FrameReaderServ createFrameReaderCli(ByteBuffer bb) {
-//		var reader = new FrameReaderServ(bb);
-//		return reader;
-//	}
 
 	
 	@Override
@@ -45,56 +55,64 @@ public class FrameReader implements Reader{
 			throw new IllegalStateException();
 		}
 		bb.flip();
-
+		
 		if (state == State.WAITING_OPCODE && bb.remaining() >= Byte.BYTES) {
 			opCode = bb.get();
 			state = State.WAITING_TRAME;
 		}
-		ProcessStatus status;
-		switch (opCode) {
-		case 0:
-			status = connectionReader.process();
-			if (status != ProcessStatus.DONE) {
-				bb.compact();
-				return status;
-			} else {
-				state = State.DONE;
-				frame = (Frame) connectionReader.get();
-				return ProcessStatus.DONE;
-			}
-		case 1: 
-			status = publicMessageReader.process();
-			if (status != ProcessStatus.DONE) {
-				bb.compact();
-				return status;
-			} else {
-				state = State.DONE;
-				frame = (Frame) publicMessageReader.get();
-				return ProcessStatus.DONE;
-			}
-		case 2: 
-			status = privateRequestReader.process();
-			if (status != ProcessStatus.DONE) {
-				bb.compact();
-				return status;
-			} else {
-				state = State.DONE;
-				frame = (Frame) privateRequestReader.get();
-				return ProcessStatus.DONE;
-			}
-		case 3: 
-			status = privateAnswerReader.process();
-			if (status != ProcessStatus.DONE) {
-				bb.compact();
-				return status;
-			} else {
-				state = State.DONE;
-				frame = (Frame) privateAnswerReader.get();
-				return ProcessStatus.DONE;
-			}
-		default:
-			return ProcessStatus.ERROR;
+		
+		if(state == State.WAITING_TRAME) {
+			return map.get(opCode).get();
 		}
+		
+		return ProcessStatus.REFILL;
+		
+		
+//		ProcessStatus status;
+//		switch (opCode) {
+//		case 0:
+//			status = connectionReader.process();
+//			if (status != ProcessStatus.DONE) {
+//				bb.compact();
+//				return status;
+//			} else {
+//				state = State.DONE;
+//				frame = (Frame) connectionReader.get();
+//				return ProcessStatus.DONE;
+//			}
+//		case 1: 
+//			status = publicMessageReader.process();
+//			if (status != ProcessStatus.DONE) {
+//				bb.compact();
+//				return status;
+//			} else {
+//				state = State.DONE;
+//				frame = (Frame) publicMessageReader.get();
+//				return ProcessStatus.DONE;
+//			}
+//		case 2: 
+//			status = privateRequestReader.process();
+//			if (status != ProcessStatus.DONE) {
+//				bb.compact();
+//				return status;
+//			} else {
+//				state = State.DONE;
+//				frame = (Frame) privateRequestReader.get();
+//				return ProcessStatus.DONE;
+//			}
+//		case 3: 
+//			status = privateAnswerReader.process();
+//			if (status != ProcessStatus.DONE) {
+//				bb.compact();
+//				return status;
+//			} else {
+//				state = State.DONE;
+//				frame = (Frame) privateAnswerReader.get();
+//				return ProcessStatus.DONE;
+//			}
+//		default:
+//			return ProcessStatus.ERROR;
+//		}
 	}
 
 	@Override
@@ -123,8 +141,20 @@ public class FrameReader implements Reader{
 		case 3: 
 			privateAnswerReader.reset();
 			break;
-		default:
-			throw new IllegalStateException("Non valid Op_Code");
+		
+		}
+	}
+	
+	
+	private ProcessStatus processReader(Reader reader) {
+		var status = reader.process();
+		if (status != ProcessStatus.DONE) {
+			bb.compact();
+			return status;
+		} else {
+			state = State.DONE;
+			frame = (Frame) reader.get();
+			return ProcessStatus.DONE;
 		}
 	}
 }
